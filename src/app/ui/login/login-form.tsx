26a7bc82aec82ed43c7d58d40ca6790d117de648
IkkiKyomoto <kazu06851@gmail.com>
2024-07-13 21:42:50 +0900

    fix:login page

--- src/app/ui/login/login-form.tsx
+++ src/app/ui/login/login-form.tsx
@@ -4,17 +4,17 @@ import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 
 export default function LoginForm() {
-  const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callbackUrl") || "/profile";
+  // const searchParams = useSearchParams();
+  // const callbackUrl = searchParams.get("callbackUrl") || "/profile";
   return (
     <div>
       <form>
-        <label htmlFor="email">メールアドレス</label>
+        {/* <label htmlFor="email">メールアドレス</label>
         <input type="email" name="email" id="email" />
         <label htmlFor="password">パスワード</label>
         <input type="password" name="password" id="password" />
         <button type="submit">ログイン</button>
-        <button onClick={() => signIn('google', {callbackUrl})}>Google</button>
+        <button onClick={() => signIn('google', {callbackUrl})}>Google</button> */}
       </form>
     </div>
   );

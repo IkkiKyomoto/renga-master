3152036e0e0ca9609fd2ad9937e2e82d462a3a12
IkkiKyomoto <kazu06851@gmail.com>
2024-07-16 14:34:15 +0900

    edit: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default NextAuth(authOptions).auth
 
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/login", request.url));
-}
+// export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL("/login", request.url));
+// }
 
 export const config = {
   matcher: ["/hokku/:path*", "/tsukeku/:path*", "/mypage/:path*"],

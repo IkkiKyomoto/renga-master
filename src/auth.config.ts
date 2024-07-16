b9873eb10e30448aad3c2c31b549a36eff09b09b
IkkiKyomoto <kazu06851@gmail.com>
2024-07-16 15:28:47 +0900

    delete: vercel.json

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,9 @@
 import type { NextAuthConfig } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import { z } from "zod";
-import {User} from '@/app/lib/definitions';
-import prisma from '@/app/lib/prisma';
-import {passwordMatch} from '@/app/lib/hash';
+// import Credentials from "next-auth/providers/credentials";
+// import { z } from "zod";
+// import {User} from '@/app/lib/definitions';
+// import prisma from '@/app/lib/prisma';
+// import {passwordMatch} from '@/app/lib/hash';
 
 export const authConfig = {
   pages: {

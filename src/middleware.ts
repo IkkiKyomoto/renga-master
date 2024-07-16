133040201e1affb726754922c12fe2828423cde9
IkkiKyomoto <kazu06851@gmail.com>
2024-07-16 11:34:19 +0900

    add: encrypt

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth";
 import { authOptions } from "./auth.config";
+import { NextRequest, NextResponse } from "next/server";
 
 export default NextAuth(authOptions).auth
 
-// export function middleware(request: NextRequest) {
-//   return NextResponse.redirect(new URL("/login", request.url));
-// }
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL("/login", request.url));
+}
 
 export const config = {
   matcher: ["/hokku/:path*", "/tsukeku/:path*", "/mypage/:path*"],

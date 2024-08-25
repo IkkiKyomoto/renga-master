bd3eb378cd0bcdf64458f62e729cc05beac2211b
IkkiKyomoto <kazu06851@gmail.com>
2024-08-25 17:04:04 +0900

    fix: createTsukeku

--- src/app/tsukeku/[id]/create/page.tsx
+++ src/app/tsukeku/[id]/create/page.tsx
@@ -7,25 +7,21 @@ import { notFound } from "next/navigation";
 import { auth } from "@/auth";
 import { Suspense } from "react";
 
-export default async function Page() {
+export default async function Page({ params }: { params: { ikku: string; niku: string, sanku:string, description:string } }) {
   const session = await auth();
-  const searchParams = useSearchParams();
-  const ikku = searchParams.get("ikku") as string;
-  const niku = searchParams.get("niku") as string;
-  const sanku = searchParams.get("sanku") as string;
-  const description = searchParams.get("description") as string;
 
-  if (!(ikku && niku && sanku)) {
+
+  if (!(params.ikku && params.niku && params.sanku)) {
     notFound();
   }
   return (
     <div className="flex flex-col gap-6 items-center justify-center mb-12">
       <Suspense fallback={<div>Loading...</div>}>
         <HokkuCard
-          ikku={ikku}
-          niku={niku}
-          sanku={sanku}
-          description={description}
+          ikku={params.ikku}
+          niku={params.niku}
+          sanku={params.sanku}
+          description={params.description}
           isPosted={false}
         />
       </Suspense>

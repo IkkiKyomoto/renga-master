708577ff34c7c104e32d7fd16b71e701081af512
IkkiKyomoto <kazu06851@gmail.com>
2024-08-25 16:01:04 +0900

    fix hokku

--- src/app/hokku/page.tsx
+++ src/app/hokku/page.tsx
@@ -1,16 +1,19 @@
-"use client";
+"use server";
 
 import React from "react";
 import HokkuForm from "@/app/ui/hokku/hokku-form";
-import { useSession } from "next-auth/react";
+import { auth } from "@/auth";
+import { Suspense } from "react";
 
-export default function Page() {
-  const { data: session } = useSession();
+export default async function Page() {
+  const session = await auth()
   return (
     <div className="h-screen">
       <h1 className="text-center text-3xl font-bold mb-6 mt-6">発句する</h1>
       <div>
-        <HokkuForm session={session} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <HokkuForm session={session} />
+        </Suspense>
       </div>
     </div>
   );
